3dfb07da4c9a1acfb9f80244e9cc7b1a86778508
0030481621019 <0030481621019@LTI6404.lti.fatecso.edu.br>
2018-05-07 11:39:31 -0300

    jkhkhkjh

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,30 @@
+package sistema.modelos;
+
+public class Professor {
+	
+	private int numero;
+	private String nome;
+	private String materia;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMateria() {
+		return materia;
+	}
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+	
+	
+
+}
